b4dad764c2057ee94227b75e1f7201a731e627e9
sunfb <sunfb@asiainfo.com>
2015-08-25 21:38:16 +0800

    在分支上改变代码

--- git_test/src/com/ai/test/HelloGit.java
+++ git_test/src/com/ai/test/HelloGit.java
@@ -5,6 +5,7 @@ import java.util.Date;
 public class HelloGit {
 
 	public static void main(String[] args) {
+		System.out.println("This is a develop!");
 		System.out.println("Hello"+new Date());
 	}
 }
